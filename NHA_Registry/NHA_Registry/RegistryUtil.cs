736a08f99c59843567b45cb42963d279bba348a0
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2022-10-05 19:41:49 +1030

    Add files via upload

---
+++ NHA_Registry/NHA_Registry/RegistryUtil.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.Win32;
+
+namespace NHA_Registry{
+public static class RegistryUtil{
+
+public static uint GetRegistryUInt32Value(string Path,string ValueName,uint Default) =>
+(uint)Registry.GetValue(Path,ValueName,Default);   
+
+        
+public static string GetRegistryStringValue(string Path,string ValueName,string Default) =>
+(string)Registry.GetValue(Path,ValueName,Default);   
+
+}
+
+}
